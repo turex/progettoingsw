f9e41015d47af8aa643d22f9cabbc37d32bf0d60
turex <salvo95ct@gmail.com>
2024-01-14 02:39:33 +0100

    ComandiPrenotazione : Add checkPrenotazione function

--- src/main/ComandiPrenotazione.java
+++ src/main/ComandiPrenotazione.java
@@ -9,6 +9,7 @@ public class ComandiPrenotazione {
 	
 	
 	void addPrenotazione(PrenotazioneBuilder p) {       
+		
 		pren.add(p.getPrenotazione());
 
 	}
@@ -19,6 +20,40 @@ public class ComandiPrenotazione {
 		//System.out.println(medi.get(z));
 	}
 	
+boolean checkPrenotazione(String nome_paziente, String cognome_paziente, String nome_medico, String cognome_medico, String professione , String data_prenotazione) {
+		
+		// True se esiste o ci sono errori
+				
+		if (nome_paziente.isEmpty() || nome_medico.isEmpty() || professione.isEmpty() || data_prenotazione.isEmpty())
+			return true;
+		
+		//TODO :: Implement cognome of paziente and medico
+		for (int i = 0; i< pren.size(); i++) {
+			String check_nomepaziente="";
+			String check_nomemedico="";
+			String check_professione="";
+			String check_dataprenotazione="";
+			
+			
+			check_nomepaziente = pren.get(i).nome_paziente.toUpperCase();
+			check_nomemedico = pren.get(i).nome_medico.toUpperCase();
+			check_professione = pren.get(i).professione.toUpperCase();
+			check_dataprenotazione = pren.get(i).data;
+			
+			if(check_nomepaziente.equals(nome_paziente.toUpperCase()) && check_nomemedico.equals(nome_medico.toUpperCase()) && 
+					check_professione.equals(professione.toUpperCase()) && check_dataprenotazione.equals(data_prenotazione))
+			
+				return true;
+			
+			
+		}
+			return false;
+		
+		
+	
+
+}
+	
 	void listPrenotazioni(String nome_paziente) {  
 		
 		for (int list = 0; list < pren.size(); list++) {
@@ -27,8 +62,11 @@ public class ComandiPrenotazione {
 			
 			if(check_nome.equals(nome_paziente.toUpperCase()))
 				System.out.println(pren.get(list));
+			//else
+			//	System.out.println("Lista vuota");
 			
 		}
+		
 	}
 
 
