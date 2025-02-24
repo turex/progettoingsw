d62a762025e52972bbd8685c8600982fe3241c54
turex <salvo95ct@gmail.com>
2025-02-24 23:50:23 +0100

    Update\

--- src/test/java/org/progettingsw/OCM/AppTest.java
+++ src/test/java/org/progettingsw/OCM/AppTest.java
@@ -2,7 +2,7 @@ package org.progettingsw.OCM;
 
 import static org.junit.Assert.assertEquals;
 
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,6 +13,9 @@ public class AppTest
 	MedicoBuilder a = new MedicoBuilder();
 	PazienteBuilder b = new PazienteBuilder();
 	PrenotazioneBuilder c = new PrenotazioneBuilder();
+	Basket d = Basket.getIstance();
+	Pagamento e = new Pagamento();
+	Pagamenti strategia;
 
 	@Before
 	public void setup() {
@@ -22,7 +25,7 @@ public class AppTest
 		System.out.println("Building medico");
 		a.setNome("Test").setCognome("Uno").setID("RAND").setProfessione("Medico").getMedico();
 		System.out.println("Building prenotazione");
-		
+		c.setidPaziente("ABCDEF5490J").setidMedico("01234567").setPriority("Media").setData("12/02/2025 09:00").setCosto(60).getPrenotazione();
 		
 	}
 	
@@ -36,9 +39,27 @@ public class AppTest
     	System.out.println("Inizio test del building di 'Medico'");
     	System.out.println("Nome medico : " + a.getMedico().getNome());
     	assertEquals("Test", a.getMedico().getNome());
+    	System.out.println("Inizio test del building di 'Prenotazione'");
+    	System.out.println("Nome medico : " + c.getPrenotazione().getidPaziente());
+    	assertEquals("ABCDEF5490J", c.getPrenotazione().getidPaziente());
+    	d.addToBasket("OCULISTA", "ABCDEF5490J", "MEDIO", "12/02/1980");
+    	System.out.println("Totale prima di aver pagato : " + d.getTotale("ABCDEF5490J"));
+    	strategia = new pagaPayPal();
+    	e.pagato(strategia, "ABCDEF5490J");
+    	System.out.println("Totale dopo aver pagato : " + d.getTotale("ABCDEF5490J"));
     	
+    	System.out.println("Check esenzione > 65 anni");
+    	
+    	d.addToBasket("OCULISTA", "ABCDEF5490J55", "ALTO", "12/02/1940");
     }
     
+    @After
+    public void clean() {
+    System.out.println("Cleaning build");	
+    d.clearBasket("ABCDEF5490J");
+    
+    System.out.println("Basket cleaned");	
+    }
   
     
     
